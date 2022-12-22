f4b28a6fccb588819ef0802a60eafcf6081e1fd7
MASHOD0 <mashhood2002@gmail.com>
2022-12-22 19:21:39 +0530

    add SummaryGenerator.py

---
+++ SummaryGenerator.py
@@ -0,0 +1,23 @@
+from newspaper import Article
+
+
+class Summary_Generator:
+    """
+    This class generates the summary of the news article
+    """
+    def __init__(self, url, lang):
+        self.url = url
+        self.lang = lang
+        self.summary = self.generate_summary()
+
+    def generate_summary(self):
+        """
+        generates summary of the news article
+        """
+        article = Article(self.url, language=self.lang)
+        article.download()
+        article.parse()
+        article.nlp()
+        summary = article.summary
+        
+        return summary
